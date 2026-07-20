5633f51125e1f24ae96a91ced0c0cf2a4cd0a197
haricharan39 <haricharan25039@gmail.com>
2026-07-20 12:09:16 +0530

    CS50P Lecture 0: Functions and Variables

---
+++ programming/Python/cs50p/lecture0_functions_variables/input.py
@@ -0,0 +1,12 @@
+name = input("What's Your Name?\n")
+print("Hello, " + name + "!")
+#printing double quotes inside a string using single quotes
+print('Hello my dear "friend"!')
+#printing double quotes inside a string using escape character
+print("Hello my dear \"friend\"!")
+#printing single quotes inside a string using double quotes
+print("Hello my dear 'friend'!")
+#printing single quotes inside a string using escape character
+print('Hello my dear \'friend\'!')
+#printing backslash inside a string using escape character
+print("Hello my dear \\friend\\!")
---
+++ programming/Python/cs50p/lecture0_functions_variables/variables.py
@@ -0,0 +1,21 @@
+# Ask the user for their age and name, then print a greeting message including their name and age.
+Age = int(input("What's Your Age?\n"))
+name = input("What's Your Name?\n")
+print("Hello, " + name + "! You are " + str(Age) + " years old.\n")
+
+#input asks the user for input and returns it as a string.
+# ask to input a decimal number and print it out
+y = float(input("Please enter a decimal number:\n"))
+print("You entered: " + str(y) + "\n")  
+
+x = 3
+y = 4
+# Add x and y and print the result
+result = x + y
+print("The sum of " + str(x) + " and " + str(y) + " is " + str(result) + ".\n")
+
+# Ask the user for their favorite color and print a message including their favorite color.
+favorite_color = input("What's your favorite color?\n")
+print("Your favorite color is " + favorite_color + ".\n")
+
+#
