4b8c0baa04028af46aa1301c863fa80bea0c8728
haricharan39 <haricharan25039@gmail.com>
2026-07-20 13:24:03 +0530

    Introduces basic calculator operations

---
+++ programming/Python/cs50p/lecture0_functions_variables/calculator.py
@@ -0,0 +1,30 @@
+# This program takes two numbers as input from the user, adds them together, and prints the result.
+x = input('first number: ')
+y = input('second number: ')
+z = x + y  
+print(z)
+sum = int(x) + int(y)
+print(sum)
+
+# calulate using float numbers 
+x = float(input('first number: '))
+y = float(input('second number: '))
+z = x + y
+print(z)
+
+# This program takes two numbers as input from the user, multiplies them together, and prints the result.
+x = input('first number: ') 
+y = input('second number: ')
+z = int(x) * int(y)
+print(z)
+
+# This program takes two numbers as input from the user, divides them together, and prints the result.
+x = float(input('first number: '))
+y = float(input('second number: '))
+z = x / y
+print(z)
+
+x = float(input('first number: '))
+y = float(input('second number: '))
+z = round(x, 2 )+ round(y, 2)
+print(z)
