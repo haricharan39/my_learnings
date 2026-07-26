0fd693a9c92b7ccc4ed35d832a16b191b93f649d
haricharan39 <haricharan25039@gmail.com>
2026-07-26 18:43:14 +0530

    Adds script for number comparison
    Introduces a Python script that prompts the user for two integers and uses conditional statements to determine and print which number is greater or if they are equal. This serves as an example for basic conditional logic.

---
+++ programming/Python/cs50p/lecture1_conditionals/compare.py
@@ -0,0 +1,8 @@
+x = int(input("x: "))
+y = int(input("y: "))
+if x > y:
+    print("x is greater than y")  # x is greater than y
+elif y > x:
+    print("y is greater than x")    # y is greater than x
+else:
+    print("x and y are equal")   # x and y are equal
