822652bf0b0d17a24e94da11ed7b8879e06d39db
haricharan39 <haricharan25039@gmail.com>
2026-08-02 23:17:22 +0530

    conditionals

---
+++ programming/Python/cs50p/lecture1_conditionals/evenodd.py
@@ -0,0 +1,7 @@
+print("Even" if int(input("Enter a number: ")) % 2 == 0 else "Odd") #print whether the number is even or odd
+#or
+n = int(input("Enter a number: "))
+if n % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
